4260bc1bf2dd02c797a3f80e7b9a967b77ab4f86
Mukund Sutar <87423576+mukundsutar@users.noreply.github.com>
2024-08-02 16:21:40 +0530

    testing

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({ base: "/renting-wale", plugins: [react()], }) 
+export default defineConfig({ base: "/", plugins: [react()] });
